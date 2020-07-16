ed0c1ebffb94345585ec3261bd8b637bdbad65b8
SIST <SIST@DESKTOP-P4H9LOP>
2020-07-16 17:51:48 +0900

    AAAAAA

---
+++ 20200713-접근지정어/src/접근지정어.java
@@ -0,0 +1,61 @@
+import lombok.Getter;
+import lombok.Setter;
+
+// 변수 2가지 기능 => 읽기(저장된 데이터 읽기) , 쓰기(메모리에 저장)
+
+/*
+ * 		======= 메소드 =======
+ * 		읽기 :  getter() => 저장된 데이터 값 읽기
+ * 		쓰기 :  setter() => 변수값을 저장 
+ * 
+ * 		 private String name;
+ * 			
+ * 		==> setter
+ * 			public void setName(String name)
+ * 			{
+ * 				this.name=name;
+ * 			}
+ * 			
+ * 		==> getter
+ * 			public String gerName()
+ * 			{
+ * 				return name;
+ * 			}
+ */
+
+//@Getter
+//@Setter
+class Student{
+	private int hakbun;
+	private String name;
+	private String sex;
+//	public int getHakbun() {
+//		return hakbun;
+//	}
+//	public void setHakbun(int hakbun) {
+//		this.hakbun = hakbun;
+//	}
+//	public String getName() {
+//		return name;
+//	}
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//	public String getSex() {
+//		return sex;
+//	}
+//	public void setSex(String sex) {
+//		this.sex = sex;
+//	}
+	
+		
+	}
+
+public class 접근지정어 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
